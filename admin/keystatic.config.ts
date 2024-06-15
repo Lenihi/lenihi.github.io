855c8ea8475e74e03a784f4da1f4dae70cc6c1f4
achtaitaipai <charles.cailleteau@gmail.com>
2024-06-15 14:58:01 +0200

    Create keystatic.config.ts

---
+++ admin/keystatic.config.ts
@@ -0,0 +1,41 @@
+import { config, fields, collection } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "cloud",
+    pathPrefix: "site",
+  },
+  cloud: {
+    project: "lena/lena",
+  },
+  collections: {
+    projects: collection({
+      label: "Projects",
+      slugField: "title",
+      path: "src/content/projects/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        date: fields.date({ label: "Date", validation: { isRequired: true } }),
+        priority: fields.number({
+          label: "Number",
+          validation: { isRequired: true },
+          defaultValue: 0,
+        }),
+        content: fields.mdx({
+          label: "Content",
+          extension: "md",
+          options: {
+            table: false,
+            code: false,
+            codeBlock: false,
+            image: {
+              directory: "src/assets/images/projects",
+              publicPath: "src/assets/images/projects/",
+            },
+          },
+        }),
+      },
+    }),
+  },
+});
