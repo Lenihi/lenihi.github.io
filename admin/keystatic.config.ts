2f9af5af8f18f4937311d11f201ba201606b699a
achtaitaipai <charles.cailleteau@gmail.com>
2024-06-15 15:02:24 +0200

    Update keystatic.config.ts

--- admin/keystatic.config.ts
+++ admin/keystatic.config.ts
@@ -37,5 +37,33 @@ export default config({
         }),
       },
     }),
+    instants: collection({
+      label: "Instants",
+      slugField: "title",
+      path: "src/content/instants/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        date: fields.date({ label: "Date", validation: { isRequired: true } }),
+        priority: fields.number({
+          label: "Number",
+          validation: { isRequired: true },
+          defaultValue: 0,
+        }),
+        content: fields.mdx({
+          label: "Content",
+          extension: "md",
+          options: {
+            table: false,
+            code: false,
+            codeBlock: false,
+            image: {
+              directory: "src/assets/images/instants",
+              publicPath: "src/assets/images/instants/",
+            },
+          },
+        }),
+      },
+    }),
   },
 });
